e4a5d7fb3ee8c08fb1748bec25e4ac6a74f67a1b
Sergei Pradko <rodzy28@yandex.ru>
2023-06-25 16:49:21 +0300

    Исправление ошибок Теста_2

--- models/user.js
+++ models/user.js
@@ -6,16 +6,19 @@ const userSchema = new mongoose.Schema({
     require: true,
     minLength: 2,
     maxLength: 30,
+    default: 'Name',
   },
   about: {
     type: String,
     require: true,
     minLength: 2,
     maxLength: 30,
+    default: 'About',
   },
   avatar: {
     type: String,
     require: true,
+    default: 'https://partnerkin.com/storage/files/file_1640014534_1.webp',
   },
 });
 
