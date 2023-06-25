013b0268e6892b6bb21f7432f62c9d495ceacf9d
Sergei Pradko <rodzy28@yandex.ru>
2023-06-25 08:10:04 +0300

    Create Models

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('Hello!');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+app.use(express.json());
+
+const users = [];
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017/mestodb', {
+    useNewUrlParser: true,
+  });
+
+app.get('/users', (req, res) => {
+  res.status(200).send(users);
+});
+
+app.post('/users', (req, res) => {
+  users.push(req.body);
+  res.status(200).send(users);
+});
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+});
