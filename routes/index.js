55a283886618ee5266981dd869161d4115e15935
Sergei Pradko <rodzy28@yandex.ru>
2023-06-25 16:44:48 +0300

    Исправление ошибок Теста_2

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 
-router.use('*', (req, res) => {
-  res.status(404).send('Page Not Found');
-});
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
+router.use('/*', (req, res) => {
+  res.status(404).send('Page Not Found');
+});
 
 module.exports = router;
